03fcba6937fa83859efaaae6af396f342b8ec529
Alekseypavlov14 <aleshapavlov9@gmail.com>
2024-02-19 22:20:43 +0200

    fixed bug with menu

--- src/features/menu/components/MobileMenuNavigation/MobileMenuNavigation.tsx
+++ src/features/menu/components/MobileMenuNavigation/MobileMenuNavigation.tsx
@@ -1,4 +1,7 @@
+'use client'
+
 import { FC } from 'react'
+import { closeSelector, useMenuStore } from '../../store'
 import { navbarLinksConfig } from '@/configs/routing'
 import styles from './MobileMenuNavigation.module.scss'
 import Link from 'next/link'
@@ -6,10 +9,17 @@ import Link from 'next/link'
 interface MobileMenuNavigationProps {}
 
 export const MobileMenuNavigation: FC<MobileMenuNavigationProps> = () => {
+  const closeMenu = useMenuStore(closeSelector)
+
   return (
     <div className={styles.MobileMenuNavigation}>
       {navbarLinksConfig.map(link => (
-        <Link key={link.url} className={styles.Link} href={link.url}>
+        <Link 
+          className={styles.Link} 
+          onClick={closeMenu} 
+          href={link.url}
+          key={link.url} 
+        >
           {link.label}
         </Link>
       ))}
