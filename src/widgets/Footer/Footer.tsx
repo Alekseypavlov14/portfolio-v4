2554a37fb70293b6eeb5c4dca3d1f444c8321c21
Alekseypavlov14 <aleshapavlov9@gmail.com>
2024-03-22 14:48:19 +0200

    applied Column shared component for Footer

--- src/widgets/Footer/Footer.tsx
+++ src/widgets/Footer/Footer.tsx
@@ -2,9 +2,9 @@ import { FC } from 'react'
 import { buttonOutlinedVariant, buttonBigSize } from '@/shared/components/Button'
 import { CollaborateButton } from '@/features/collaboration'
 import { FooterNavigation } from './components/FooterNavigation'
+import { Column, Columns } from '@/shared/components/Columns'
 import { Contacts } from '../Contacts'
 import { Section } from '@/shared/components/Section'
-import { Columns } from '@/shared/components/Columns'
 import { Logo } from '@/shared/components/Logo'
 import styles from './Footer.module.scss'
 
@@ -14,18 +14,18 @@ export const Footer: FC<FooterProps> = () => {
   return (
     <footer className={styles.Footer}>
       <Section className={styles.FooterSection}>
-        <Columns>
-          <div className={styles.FooterColumn}>
+        <Columns columns={3}>
+          <Column className={styles.FooterColumn}>
             <Logo />
   
             <Contacts className={styles.Contacts} />
-          </div>
+          </Column>
   
-          <div className={styles.FooterColumn}>
+          <Column className={styles.FooterColumn}>
             <FooterNavigation />
-          </div>
+          </Column>
   
-          <div className={styles.FooterColumn}>
+          <Column className={styles.FooterColumn}>
             <CollaborateButton
               className={styles.FooterCollaborateButton}
               variant={buttonOutlinedVariant}
@@ -33,7 +33,7 @@ export const Footer: FC<FooterProps> = () => {
             />
   
             <Contacts className={styles.Contacts} />
-          </div>
+          </Column>
         </Columns>
 
         <div className={styles.Copyright}>&copy; Oleksii Pavlov 2024</div>
