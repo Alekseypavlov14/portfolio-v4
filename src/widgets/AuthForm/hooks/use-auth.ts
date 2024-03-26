4154c7b2201e2286eb11b06fda24854aa6d8d623
Alekseypavlov14 <aleshapavlov9@gmail.com>
2024-03-26 19:58:41 +0200

    added clearing AuthForm Input after submitting

--- src/widgets/AuthForm/hooks/use-auth.ts
+++ src/widgets/AuthForm/hooks/use-auth.ts
@@ -1,16 +1,20 @@
 'use client'
 
-import { sessionIdSelector, useAuthStore } from '../store'
+import { sessionIdSelector, updateSessionIdSelector, useAuthStore } from '../store'
 import { sessionsSessionStorage } from '@/entities/sessions/client'
 import { useNavigation } from '@/services/navigation'
 
 
 export function useAuth() {
   const sessionId = useAuthStore(sessionIdSelector)
+  const updateSessionId = useAuthStore(updateSessionIdSelector)
+
   const { navigateAdminPage } = useNavigation()
 
   return () => {
     sessionsSessionStorage.setItem(sessionId)
     navigateAdminPage()
+    
+    updateSessionId('')
   }
 }
