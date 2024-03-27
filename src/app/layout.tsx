69d1a40aa394f22e5a74b82aa0d9fc0dc2bb032b
Alekseypavlov14 <aleshapavlov9@gmail.com>
2024-03-27 11:38:20 +0200

    added fetchCache: default-no-store to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,9 @@ export const metadata: Metadata = {
   title: "Oleksii Pavlov | Frontend Developer"
 }
 
+// prevents fetch cache by default
+export const fetchCache = 'default-no-store'
+
 export default function RootLayout({
   children,
 }: Readonly<{
