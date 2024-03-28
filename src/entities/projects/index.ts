c2df1eb9eaebf6933e84e85a3da2fa6abe095544
Alekseypavlov14 <aleshapavlov9@gmail.com>
2024-03-28 21:34:35 +0200

    updated LoaderSpinner component

--- src/entities/projects/index.ts
+++ src/entities/projects/index.ts
@@ -1,4 +1,4 @@
-export type { ProjectEntity } from './project.entity'
+export type { ProjectEntity, ProjectData } from './project.entity'
 
 export { isProjectRepositoryPrivate } from './utils/is-project-repository-private'
 export { isProjectUnpublished } from './utils/is-project-unpublished'
